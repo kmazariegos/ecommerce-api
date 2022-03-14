97fde9d1391f54ea72f4d685132fa780bb9934d6
Karla <mazariek@gmail.com>
2022-03-14 12:44:33 -0700

    commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose")
+
+mongoose.connect(
+"mongodb+srv://ecommerce-store:ecommerce-store@cluster0.bflxl.mongodb.net/ecommerce-store?retryWrites=true&w=majority"
+)
+.then(()=>{console.log("DBConnect success")})
+.catch((err)=>{console.log(err)})
+
+app.listen(5000, ()=>{
+    console.log("backend server is running");
+})
+
