c6c09c3e81d10e249075a8355645fe83c686bffd
Karla <mazariek@gmail.com>
2022-03-15 14:44:03 -0700

    commit

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose")
+const dotenv = require('dotenv')
+const userRoute = require('./routes/user')
 
-mongoose.connect(
-"mongodb+srv://ecommerce-store:ecommerce-store@cluster0.bflxl.mongodb.net/ecommerce-store?retryWrites=true&w=majority"
-)
+dotenv.config()
+
+mongoose
+.connect(process.env.MONGO_URL)
 .then(()=>{console.log("DBConnect success")})
 .catch((err)=>{console.log(err)})
 
-app.listen(5000, ()=>{
+app.listen(process.env.PORT || 5000, ()=>{
     console.log("backend server is running");
 })
 
