e4f85d11ba39d9e13e3f05440d3f473a5e891f02
Karla <mazariek@gmail.com>
2022-03-16 20:00:26 -0700

    commit

--- routes/auth.js
+++ routes/auth.js
@@ -26,13 +26,17 @@ router.post("/register", async (req, res) => {
 
 //LOGIN
 
-router.post('/login', async (req, res) => {
+router.post('/login', async (req, res) => { 
     try{
         const user = await User.findOne({username: req.body.username})
+
         !user && res.status(401).json("wrong credentials")
+
         const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC)
         const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8)
+
         OriginalPassword !== req.body.password && res.status(401).json("wrong credentials")
+
         const accessToken = jwt.sign(
             {
                 id: user._id,
@@ -41,6 +45,7 @@ router.post('/login', async (req, res) => {
             process.env.JWT_SEC,
             {expiresIn:"3d"}
         )
+        
         const {password, ...other} = user._doc;
         res.status(200).json({...others, accessToken})
 
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -23,10 +23,21 @@ const verifyTokenAndAuthorization = (req, res, next) => {
         res.status(403).json("You are not alowed to do that!");
       }
     });
+};
+
+
+  const verifyTokenAndAdmin = (req, res, next) => {
+    verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+        next();
+    } else {
+        res.status(403).json("You are not alowed to do that!");
+    }
+    });
   };
-
-
-module.exports = {
+  
+  module.exports = {
     verifyToken,
-    verifyTokenAndAuthorization
+    verifyTokenAndAuthorization,
+    verifyTokenAndAdmin,
   };
